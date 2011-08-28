8b8f717d857d1d9ba105c7d415063a88651e7c2f
Ben Symonds <ben@state.it>
2011-08-28 14:15:08 +0100

    add some more basic fields

--- app/models/development.rb
+++ app/models/development.rb
@@ -3,6 +3,11 @@ class Development
   include Mongoid::Timestamps
   field :name, type: String
   field :borough, type: String
+  field :ward, type: String
+  field :location, type: String
   embeds_one :address
   accepts_nested_attributes_for :address
+  STATUSES = %w( pre-planning planning approved demolition site-prep construction complete )
+  field :status, type: String
+  validates_inclusion_of :status, :in => STATUSES
 end
