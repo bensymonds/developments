94b5137d32bc70d132e035e73e6a92bb50882965
Ben Symonds <ben@state.it>
2011-08-28 16:35:05 +0100

    make sure fields in address are required

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address
   include Mongoid::Document
-  field :postcode_out, :type => String
-  field :postcode_in, :type => String
-  field :rest, :type => String
+  field :postcode_out, :type => String, :required => true
+  field :postcode_in, :type => String, :required => true
+  field :rest, :type => String, :required => true
   embedded_in :development
 end
