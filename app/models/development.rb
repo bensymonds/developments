0572384121c2e8f09a1e977e757e0e63d9d4498a
Ben Symonds <ben@state.it>
2011-08-28 14:17:19 +0100

    use old style hash syntax

--- app/models/development.rb
+++ app/models/development.rb
@@ -1,13 +1,13 @@
 class Development
   include Mongoid::Document
   include Mongoid::Timestamps
-  field :name, type: String
-  field :borough, type: String
-  field :ward, type: String
-  field :location, type: String
+  field :name, :type => String
+  field :borough, :type => String
+  field :ward, :type => String
+  field :location, :type => String
   embeds_one :address
   accepts_nested_attributes_for :address
   STATUSES = %w( pre-planning planning approved demolition site-prep construction complete )
-  field :status, type: String
+  field :status, :type => String
   validates_inclusion_of :status, :in => STATUSES
 end
