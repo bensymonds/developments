e86ce24dc43ea6af20b027b9f3f4acb8d6a01192
Ben Symonds <ben@state.it>
2011-08-28 16:37:15 +0100

    require some fields in development model

--- app/models/development.rb
+++ app/models/development.rb
@@ -1,9 +1,9 @@
 class Development
   include Mongoid::Document
   include Mongoid::Timestamps
-  field :name, :type => String
-  field :borough, :type => String
-  field :ward, :type => String
+  field :name, :type => String, :required => true
+  field :borough, :type => String, :required => true
+  field :ward, :type => String, :required => true
   field :location, :type => String
   embeds_one :address
   accepts_nested_attributes_for :address
