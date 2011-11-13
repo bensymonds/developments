082bbd3350f5355c66c5636f316e86f2d7c185c3
Ben Symonds <ben@state.it>
2011-11-13 23:10:34 +0000

    companies scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Developments::Application.routes.draw do
   resources :developments
+  resources :companies
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
