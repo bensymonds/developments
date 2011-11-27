63ad8238857fc83e674670f135c3e0b6b1665c81
Ben Symonds <ben@state.it>
2011-11-27 22:52:07 +0000

    add gems for assets compilation

--- Gemfile
+++ Gemfile
@@ -6,6 +6,12 @@ gem "mongoid"
 gem "bson_ext"
 gem "haml"
 
+group :assets do
+  gem 'sass-rails',   "~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
 group :development, :test do
   gem 'wirble'
   gem 'ruby-debug19', :require => 'ruby-debug'
--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "sprockets/railtie"
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+Bundler.require *Rails.groups(:assets => %w(development test)) if defined?(Bundler)
 
 module Developments
   class Application < Rails::Application
