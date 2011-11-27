9e6334eb5019e8954c48ab94c88387edebc655d8
Ben Symonds <ben@state.it>
2011-11-27 21:56:17 +0000

    :url should be required

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link
   include Mongoid::Document
-  field :url, :type => String
+  field :url, :type => String, :required => true
   field :note, :type => String
   embedded_in :development
 end
